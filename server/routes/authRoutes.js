3b70169f625b69b3a0e4171f9c9833241914c785
shraddha7935 <shraddhak21040@gmail.com>
2026-04-28 01:13:02 +0530

    Fix case-sensitive error

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
